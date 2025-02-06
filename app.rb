63d245fc89838bd603ff2ce2b1ec7e68aa1c2138
jnorton-7 <jnorton2@chicagobooth.edu>
2025-02-06 03:06:25 +0000

    got process roll to work

--- app.rb
+++ app.rb
@@ -8,12 +8,14 @@ get("/") do
   "
 end
 
-get("process_roll") do
+get("/process_roll") do
 @num_dice = params.fetch("dice").to_i
 @num_sides = params.fetch("sides").to_i
 
+@rolls = []
+
 @num_dice.times do
-  @rolls.push(rand.(1..@num_sides))
+  @rolls.push(rand(1..@num_sides))
 end
 
   erb(:results)
